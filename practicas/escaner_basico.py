78cf49e4fa2d14704a3e68948425137cd7b56b3e
flaco-pixel <angeljaviertorres519@gmail.com>
2026-09-22 23:21:09 +0000

    guardar los cambios

--- practicas/escaner_basico.py
+++ practicas/escaner_basico.py
@@ -4,7 +4,7 @@ import socket
 objetivo = "127.0.0.1"
 puertos = [21, 22, 80, 443, 8080]
 
-print(f"--- ESCANEANDO OBJETIVO REAL: {objetivo} ---"
+print(f"--- ESCANEANDO OBJETIVO REAL: {objetivo} ---")
 
 for puerto in puertos:
     # Creamos un socket de tipo IPv4 (AF_INET) y TCP (SOCK_STREAM)
